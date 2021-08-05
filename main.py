3d836d001063e18a140cadbaf0488b342e6f0724
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-08-05 19:09:05 -0300

    Rename methods

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import json
 
-def cipher_plaintext(key, plaintext):
+def encrypt_plaintext(key, plaintext):
     keystream = generate_keystream(key, plaintext)
-    ciphered_chars = []
+    encrypted_chars = []
 
     for i in range(len(plaintext)):
         new_char = find_new_chipered_char(plaintext[i], keystream[i])
-        ciphered_chars.append(new_char)
+        encrypted_chars.append(new_char)
 
-    ciphered_text = ("".join(ciphered_chars))
-    return ciphered_text
+    encrypted_text = ("".join(encrypted_chars))
+    return encrypted_text
 
 def find_new_chipered_char(keystream_char, plaintext_char):
     file = open('./alphabet_table.json')
@@ -19,9 +19,10 @@ def find_new_chipered_char(keystream_char, plaintext_char):
     position_in_alphabet = (plaintext_char_unicode + keystream_char_unicode) % 26
     return data[position_in_alphabet]
 
+
 def generate_keystream(key, plaintext):
     plaintext_length = len(plaintext)
     repeated_key = key * plaintext_length
     return repeated_key[0:plaintext_length]
 
-print(cipher_plaintext("key", "Ola"))
+print(encrypt_plaintext("key", "Ola"))
