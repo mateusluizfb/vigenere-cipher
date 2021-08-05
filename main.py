62ad0f455b0ffb224b7590698ccd31ed347e2f19
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-08-05 19:22:06 -0300

    Implement decryption

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def encrypt_plaintext(key, plaintext):
     encrypted_chars = []
 
     for i in range(len(plaintext)):
-        new_char = find_new_chipered_char(plaintext[i], keystream[i])
+        new_char = find_new_chipered_char(keystream[i], plaintext[i])
         encrypted_chars.append(new_char)
 
     encrypted_text = ("".join(encrypted_chars))
@@ -19,10 +19,30 @@ def find_new_chipered_char(keystream_char, plaintext_char):
     position_in_alphabet = (plaintext_char_unicode + keystream_char_unicode) % 26
     return data[position_in_alphabet]
 
+def decrypt_plaintext(key, encrypted_plaintext):
+    keystream = generate_keystream(key, encrypted_plaintext)
+    decrypted_chars = []
+
+    for i in range(len(encrypted_plaintext)):
+        decrypted_char = find_encrypted_char(keystream[i], encrypted_plaintext[i])
+        decrypted_chars.append(decrypted_char)
+
+    decrypted_text = ("".join(decrypted_chars))
+    return decrypted_text
+
+def find_encrypted_char(keystream_char, encrypted_char):
+    file = open('./alphabet_table.json')
+    data = json.load(file)
+    encrypted_char_unicode = ord(encrypted_char)
+    keystream_char_unicode = ord(keystream_char)
+    position_in_alphabet = (encrypted_char_unicode - keystream_char_unicode) % 26
+    return data[position_in_alphabet]
+
 
 def generate_keystream(key, plaintext):
     plaintext_length = len(plaintext)
     repeated_key = key * plaintext_length
     return repeated_key[0:plaintext_length]
 
-print(encrypt_plaintext("key", "Ola"))
+print(encrypt_plaintext("KEY", "OLA"))
+print(decrypt_plaintext("KEY", "YPY"))
